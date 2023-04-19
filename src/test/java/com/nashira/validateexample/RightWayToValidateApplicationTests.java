68bfeeab17d64a62abc75bd8c20390ab68fe2573
Giova <giovanotti1906@hotmail.com>
2023-04-18 21:18:52 -0500

    My first commit

---
+++ src/test/java/com/nashira/validateexample/RightWayToValidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nashira.validateexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightWayToValidateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
